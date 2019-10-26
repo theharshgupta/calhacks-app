759a06783ad033b655cd8e33404c1fc252d9226b
Bill Huang <billhuang0916@gmail.com>
2019-10-26 01:49:18 -0700

    Made Google Speech to Text a Function

--- gcloud_api.py
+++ gcloud_api.py
@@ -9,51 +9,20 @@ from google.cloud.speech import enums
 from google.cloud.speech import types
 from pydub import AudioSegment
 
-# Instantiates a client
-client = speech.SpeechClient()
 
-# The name of the audio file to transcribe
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'Audio_files',
-    'Anish.mp3')
-wav_file_name = os.path.join(
-    os.path.dirname(__file__),
-    'Audio_files',
-    'Anish.wav')
-convert = AudioSegment.from_mp3(file_name)
-convert.set_channels(1)
-##convert.export(wav_file_name, format="wav")
+def gcloud_speech_to_text(file_name):
+    # Instantiates a client
+    client = speech.SpeechClient()
+    # Loads the audio into memory
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
 
-# Loads the audio into memory
-with io.open(wav_file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        enable_word_time_offsets=True,
+        enable_automatic_punctuation=True,
+        language_code='en-US')
 
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    enable_word_time_offsets=True,
-    enable_automatic_punctuation=True,
-    language_code='en-US')
-
-# Detects speech in the audio file
-response = client.recognize(config, audio)
-
-# The first result includes start and end time word offsets
-result = response.results[0]
-# First alternative is the most probable result
-alternative = result.alternatives[0]
-print(u"Transcript: {}".format(alternative.transcript))
-# Print the start and end time of each word
-for word in alternative.words:
-    print(u"Word: {}".format(word.word))
-    print(
-        u"Start time: {} seconds {} nanos".format(
-            word.start_time.seconds, word.start_time.nanos
-        )
-    )
-    print(
-        u"End time: {} seconds {} nanos".format(
-            word.end_time.seconds, word.end_time.nanos
-        )
-    )
+    # Detects speech in the audio file
+    return client.recognize(config, audio)
