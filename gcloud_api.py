a6d33188c2518e1b9ee66707c85704eeb08754e8
Bill Huang <billhuang0916@gmail.com>
2019-10-26 01:12:27 -0700

    Testing Google Speech to Text

--- gcloud_api.py
+++ gcloud_api.py
@@ -2,9 +2,12 @@ import io
 import os
 
 # Imports the Google Cloud client library
+from itertools import chain
+
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
+from pydub import AudioSegment
 
 # Instantiates a client
 client = speech.SpeechClient()
@@ -12,17 +15,23 @@ client = speech.SpeechClient()
 # The name of the audio file to transcribe
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
+    'Audio_files',
+    'emotions.mp3')
+wav_file_name = os.path.join(
+    os.path.dirname(__file__),
+    'Audio_files',
+    'emotions.wav')
+convert = AudioSegment.from_mp3(file_name)
+convert.set_channels(1)
+##convert.export(wav_file_name, format="wav")
 
 # Loads the audio into memory
-with io.open(file_name, 'rb') as audio_file:
+with io.open(wav_file_name, 'rb') as audio_file:
     content = audio_file.read()
     audio = types.RecognitionAudio(content=content)
 
 config = types.RecognitionConfig(
     encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
     language_code='en-US')
 
 # Detects speech in the audio file
