48353e3214a0fb8704a75b356e793828fa63160a
Harsh Gupta <harsh@berkeley.edu>
2019-10-26 14:45:45 -0700

    layout file to use extends

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ Here we define the download and the upload folder on the server
 '''
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
 DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
+
 # The allowed extensions that can be uploaded on the webpage
 ALLOWED_EXTENSIONS = {'pdf', 'csv', 'mp3'}
 
@@ -33,11 +34,6 @@ app = Flask(__name__, static_url_path="/static")
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-
-if os.path.isfile('downloads/out.csv'):
-    os.remove('downloads/out.csv')
-if os.path.isfile('downloads/bal.csv'):
-    os.remove('downloads/bal.csv')
 
 # limit upload size upto 100mb
 app.config['MAX_CONTENT_LENGTH'] = 150 * 1024 * 1024
@@ -168,4 +164,4 @@ def get_gcloud_data(filename):
 
 
 def save_score_data():
-    p = subprocess.Popen(["cd","DeepMoji-master","examples","python3","score_texts_emojis.py"])
+    p = subprocess.Popen(["cd", "DeepMoji-master", "examples", "python3", "score_texts_emojis.py"])
