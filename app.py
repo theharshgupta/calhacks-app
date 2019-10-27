e805a43a6a470c9d7025bbcce42c5593ae639302
Bill Huang <billhuang0916@gmail.com>
2019-10-26 18:28:40 -0700

    Separated Process File

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 import datetime
 import requests
 from gcloud_api import *
+from analysis import *
 
 '''
 Here we define the download and the upload folder on the server
@@ -60,28 +61,15 @@ def index():
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 process_file(path=os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
                 # uploaded_file(filename=filename)
         return render_template('downloads.html')
     return render_template('index.html')
 
 
-def process_file(path):
-    """
-    Calls the relevant functions once the file is uploaded by the user. Right now we are calling the emotion tagging
-    function
-    :param path: path to the audio file
-    :return: Nothing
-    """
-    emotion_tagging(path=path)
-    dpeffects,clauses = get_clause_emotions(path=path)
-    tone_dict=tone_analyzer(clauses)
-
-    return render_template('page2.html', dpeffects=dpeffects,tone_dict=tone_dict)
-    # save_score_data()
-    # get_clause_emotions(path=path)
-    # the function above returns the clause/emotion dictionary which can be used to display the scripts.
-
-
+@app.route('/process/<path>', methods=["GET"])
+def process(path):
+    return process_file(path)
 
 def emptydir():
     """
@@ -102,46 +90,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def emotion_tagging(path):
-    # In this program, we are calling the url and making a get request to that using Harsh's API key
-    # We send the mp3 file we want to upload decoded audio in the body_json of the requests
-
-    """
-    This function takes the path of the audio file and calls the DeepAffects API.
-
-    :param path: path to the audio file, uploaded, present locally
-    :return: json response from the API
-    """
-    # Just for testing, we are commenting the API Calls and just returning the API Response as JSON
-    """
-    url = "https://proxy.api.deepaffects.com/audio/generic/api/v2/sync/recognise_emotion?apikey" \
-          "=7h1YbhaMje9IBTrUTDGNa8KGABD1n9cn"
-
-    headers = {'Content-Type': "application/json"}
-
-    with open(path, 'rb') as fin:
-        audio_content = fin.read()
-    audio_decoded = base64.b64encode(audio_content).decode('utf-8')
-
-    body_json = {"content": audio_decoded,
-                 "encoding": "MPEG Audio",
-                 "language_code": "en-US",
-                 "sample_rate": 48000}
-
-    # text_body_json = {"content": ""}
-
-    data = requests.post(url=url, json=body_json, headers=headers)
-    with open('audio-analysis/results.txt', 'w') as output:
-        output.write(data.text)
-    pprint(data.text)
-    """
-
-    json_result = [{"end": 3.0, "start": 0.0, "emotion": "neutral"}, {"end": 6.0, "start": 3.0, "emotion": "happy"},
-                   {"end": 8.856, "start": 6.0, "emotion": "excited"}]
-    print(json_result)
-    return json_result
-
-
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     # We are not really using the filename arg but ya
@@ -154,69 +102,5 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-def get_clause_emotions(filename):
-    # loads audio file-filename and returns clause_and_emotion dictionary.
-    gresponse = gcloud_speech_to_text(filename)
-    deepresponse = emotion_tagging(filename)
-
-    deep_affects_time_stamps = []
-    audio_emotions = []
-
-    google_words = gresponse["words"]
-    gtime_stamps = []
-    words = []
-
-    for word in google_words:
-        words.append(word["word"])
-        gtime_stamps.append(float(word['end_time']) / 1000)
-
-    for tag in deepresponse:
-        deep_affects_time_stamps.append(tag["end"])
-        audio_emotions.append(tag["emotion"])
-
-    dpeffects = {}
-    string = ''
-    pos = 0
-    l = min(len(words), len(gtime_stamps))
-    for i in range(l):
-        if pos < len(deep_affects_time_stamps):
-            t = deep_affects_time_stamps[pos]
-            if gtime_stamps[i] >= t or i == (l - 1):
-                string = string + words[i] + '.'
-                dpeffects[string] = deep_affects_time_stamps[pos]
-                string = ''
-                pos += 1
-            else:
-                string = string + words[i] + ' '
-    return dpeffects,clauses
 
 
-def answer(filename):
-    dpeffects, = get_clause_emotions(filename)
-    return json.dumps(dpeffects)
-
-
-def tone_analyzer(clauses):
-    # API KEY rNiB7aYI-pVZQ_6I-U-D_avkVNsOUUYMf9n5dXOhrjHc
-    # https://gateway.watsonplatform.net/tone-analyzer/api
-    api_key = "rNiB7aYI-pVZQ_6I-U-D_avkVNsOUUYMf9n5dXOhrjHc"
-
-    username = 'apikey'
-    password = api_key
-    watsonUrl = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21'
-    headers = {"content-type": "text/plain"}
-    data = ''
-    for clause in clauses:
-        data  = data + clause + ' '
-    try:
-        r = requests.post(watsonUrl, auth=(username, password), headers=headers, data=data)
-        print("IBM Watson \n\n\n", r.text)
-        emotions = []
-        mapping = {"joy":"joy","sadness":"neutral","fear":"neutral","disgust":"disgust","anger":"anger"}
-        for sentence in r["sentences_tone"]:
-            emotion = sentence["tones"][tone_name]
-            emotions.append(mapping[emotion])
-            #ex. watson data {"sentences_tone":[{"sentence_id":0,"text":"Ping pong is the best sport in the world.","tones":[{"score":0.822188,"tone_id":"joy","tone_name":"Joy"}]},{"sentence_id":1,"text":"I like Chinese people.","tones":[{"score":0.88939,"tone_id":"tentative","tone_name":"Tentative"}]},{"sentence_id":2,"text":"I fucking hate PG&E they are horrible and they should make changes in their management.","tones":[{"score":0.827514,"tone_id":"anger","tone_name":"Anger"}]},{"sentence_id":3,"text":"This company is bankrupt.","tones":[{"score":0.72178,"tone_id":"sadness","tone_name":"Sadness"}]}]}
-        return emotions
-    except:
-        return False
