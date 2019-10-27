cfb5da92f56da01972b1cb4f5f90a7df1bbf7b83
Bill Huang <billhuang0916@gmail.com>
2019-10-26 23:26:25 -0700

    Added Tooltips

--- static/js/displayAnalysis.js
+++ static/js/displayAnalysis.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
         data = JSON.parse(res);
         var encoding = {
             "anger": "#ad0505",
-            "joy": "#ffd500",
-            "happy": "#ffd500",
-            "excited": "#ffd500",
+            "joy": "#0a8c24",
+            "happy": "#0a8c24",
+            "excited": "#0a8c24",
             "neutral": "#77777a",
             "distgust": "#2b0505"
         };
 
         createTextProfile($("#textAnalysis"), data["text"], encoding);
         createAudioProfile($("#audioAnalysis"), data["audio"], encoding);
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
     } else {
         console.log("Error")
     }
@@ -21,8 +24,8 @@ function createTextProfile(paragraphObj, text, encoding) {
     var html = "";
     for (const [key, value] of Object.entries(text)) {
         if (value[0] in encoding) {
-            html += "<span style='color: " + encoding[value[0]] + 
-            "' data-toggle='tooltip' data-placement='top' title='" + (value[1] * 100).toFixed(2) + "% confidence'>" + key + "</span>";
+            html += "<span style='color: " + encoding[value[0]] +
+                "' data-toggle='tooltip' data-placement='top' title='" + (value[1] * 100).toFixed(2) + "% confidence'>" + key + "</span>";
         } else {
             html += "<span>" + key + "</span>";
         }
